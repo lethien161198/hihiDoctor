f2f7dc9a6b97d1fac5722895b75438b84446a6fa
Trịnh Quốc Đạt <dattrinhquoc112@gmail.com>
2021-06-01 22:55:24 +0700

    implement Prescription

---
+++ Test2/app/src/main/java/com/example/test/modules/view/prescription/PrescriptionFragment.kt
@@ -0,0 +1,14 @@
+package com.example.test.modules.view.prescription
+
+import com.example.test.R
+import com.example.test.commons.base.BaseFragment
+import com.example.test.databinding.FragmentPrescriptionBinding
+
+class PrescriptionFragment : BaseFragment<FragmentPrescriptionBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.fragment_prescription
+
+    override fun initComponent(viewBinding: FragmentPrescriptionBinding) {
+        TODO("Not yet implemented")
+    }
+}
